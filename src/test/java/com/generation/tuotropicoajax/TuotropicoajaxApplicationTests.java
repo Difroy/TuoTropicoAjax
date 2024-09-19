c8575f448aecd6010a980ebf4504d34f6a719d92
Difroy <unifroy@gmail.com>
2024-09-19 15:28:57 +0200

    git init

---
+++ src/test/java/com/generation/tuotropicoajax/TuotropicoajaxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.tuotropicoajax;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TuotropicoajaxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
